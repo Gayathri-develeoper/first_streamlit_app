5d49b62310b0e5fe461776f323d9b69d51df6d96
Gayathri-develeoper <120360991+Gayathri-develeoper@users.noreply.github.com>
2022-12-12 12:39:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title("'My Mom's New Healthy Diner'")
 
 streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & bluberry Oatmeal')
+streamlit.text('🥣 Omega 3 & bluberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
